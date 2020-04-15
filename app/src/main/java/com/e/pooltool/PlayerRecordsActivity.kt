c52f2ed6f1ebc76bff7c5bc735cd6a2777999c5c
ChaoLun Cheng <d85275@gmail.com>
2020-04-15 12:29:59 +0800

    get line chart data from saved list

--- app/src/main/java/com/e/pooltool/PlayerRecordsActivity.kt
+++ app/src/main/java/com/e/pooltool/PlayerRecordsActivity.kt
@@ -2,10 +2,12 @@ package com.e.pooltool
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -76,23 +78,24 @@ class PlayerRecordsActivity : AppCompatActivity() {
         tvPlayerName.text = name
         tvPottedRecord.text = getString(R.string.potted_record, potted)
         tvMissedRecord.text = getString(R.string.missed_record, missed)
-        tvAve.text = player.getRate().replace("%","")
+        tvAve.text = player.getRate().replace("%", "")
     }
 
+    private fun getCharData(): ArrayList<Entry> {
+        val data = ArrayList<Entry>()
+        val list = getList()
+        for (i in list.indices) {
+            val rate = list[i].getRate().replace("%","").trim().toFloat()
+            val entry = Entry(i.toFloat(),rate)
+            data.add(entry)
+        }
+        return data
+    }
+
+
+
     private fun setupLineChartData() {
-        val yVals = ArrayList<Entry>()
-        yVals.add(Entry(0f, 30f, "0"))
-        yVals.add(Entry(1f, 2f, "1"))
-        yVals.add(Entry(2f, 4f, "2"))
-        yVals.add(Entry(3f, 6f, "3"))
-        yVals.add(Entry(4f, 8f, "4"))
-        yVals.add(Entry(5f, 10f, "5"))
-        yVals.add(Entry(6f, 22f, "6"))
-        yVals.add(Entry(7f, 12.5f, "7"))
-        yVals.add(Entry(8f, 22f, "8"))
-        yVals.add(Entry(9f, 32f, "9"))
-        yVals.add(Entry(10f, 54f, "10"))
-        yVals.add(Entry(11f, 28f, "11"))
+        val yVals = getCharData()
 
         val set1: LineDataSet
         set1 = LineDataSet(yVals, "DataSet 1")
@@ -124,7 +127,7 @@ class PlayerRecordsActivity : AppCompatActivity() {
         lineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
         lineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
         //lineChart.setDrawGridBackground()
-        lineChart.xAxis.labelCount = 11
+        lineChart.xAxis.labelCount = getList().size-1
         lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 }
