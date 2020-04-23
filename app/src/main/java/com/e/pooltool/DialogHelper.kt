3de2075056a8a4399c0a436d927cf4fb37cc8e17
ChaoLun Cheng <d85275@gmail.com>
2020-04-23 19:45:44 +0800

    the dialog cannot be cancelled by touching outside area now

--- app/src/main/java/com/e/pooltool/DialogHelper.kt
+++ app/src/main/java/com/e/pooltool/DialogHelper.kt
@@ -2,33 +2,48 @@ package com.e.pooltool
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.LayoutInflater
+import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.player_name.view.*
 
 class DialogHelper(private val context: Context?, private val viewModel: MyViewModel) {
-    fun resetPlayer() {
+
+    private fun showDialog(
+        title: String,
+        msg: String,
+        posListener: DialogInterface.OnClickListener,
+        negListener: DialogInterface.OnClickListener
+    ) {
         val dialogBuilder = AlertDialog.Builder(context)
-        dialogBuilder.setTitle(context!!.getString(R.string.info))
-        dialogBuilder.setMessage(R.string.reset_score)
-        dialogBuilder.setPositiveButton(R.string.confirm) { _, _ -> viewModel.resetScores() }
-        dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> }
-        dialogBuilder.show()
+        dialogBuilder.setTitle(title)
+        dialogBuilder.setMessage(msg)
+        dialogBuilder.setPositiveButton(R.string.confirm, posListener)
+        dialogBuilder.setNegativeButton(R.string.cancel, negListener)
+        dialogBuilder.show().setCanceledOnTouchOutside(false)
+    }
+
+    fun resetPlayer() {
+        val title = context!!.getString(R.string.info)
+        val msg = context.getString(R.string.reset_score)
+        val posListener = DialogInterface.OnClickListener { _, _ -> viewModel.resetScores() }
+        val negListener = DialogInterface.OnClickListener { _, _ -> }
+        showDialog(title, msg, posListener, negListener)
     }
 
     fun deletePlayer(position: Int, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-        val dialogBuilder = AlertDialog.Builder(context)
-        dialogBuilder.setTitle(viewModel.getPlayerName(position))
-        dialogBuilder.setMessage(context!!.getString(R.string.delete))
-        dialogBuilder.setPositiveButton(R.string.confirm) { _, _ -> viewModel.removePlayer(position) }
-        dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> adapter.notifyDataSetChanged() }
-        dialogBuilder.show()
+        val title = viewModel.getPlayerName(position)
+        val msg = context!!.getString(R.string.delete)
+        val posListener =
+            DialogInterface.OnClickListener { _, _ -> viewModel.removePlayer(position) }
+        val negListener = DialogInterface.OnClickListener { _, _ -> adapter.notifyDataSetChanged() }
+        showDialog(title, msg, posListener, negListener)
     }
 
     fun deleteRecord(position: Int, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-        val dialogBuilder = AlertDialog.Builder(context)
         val record = viewModel.getDisplayedRecordsList()[position]
         val title = context!!.getString(
             R.string.rate_and_ratio,
@@ -36,35 +51,29 @@ class DialogHelper(private val context: Context?, private val viewModel: MyViewM
             record.potted,
             record.getTotal()
         )
-        dialogBuilder.setTitle(title)
-        dialogBuilder.setMessage(context.getString(R.string.delete))
-        dialogBuilder.setPositiveButton(R.string.confirm) { _, _ -> viewModel.removeRecord(position) }
-        dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> adapter.notifyDataSetChanged() }
-        dialogBuilder.show()
+        val msg = context.getString(R.string.delete)
+        val posListener =
+            DialogInterface.OnClickListener { _, _ -> viewModel.removeRecord(position) }
+        val negListener = DialogInterface.OnClickListener { _, _ -> adapter.notifyDataSetChanged() }
+        showDialog(title, msg, posListener, negListener)
     }
 
     fun savePlayer(position: Int, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-        val dialogBuilder = AlertDialog.Builder(context)
-        dialogBuilder.setTitle(viewModel.getPlayerName(position))
-        dialogBuilder.setMessage(context!!.getString(R.string.save))
-        dialogBuilder.setPositiveButton(R.string.confirm) { _, _ ->
-            viewModel.saveRecord(position)
-        }
-        dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> adapter.notifyDataSetChanged() }
-        dialogBuilder.show()
+
+        val title = viewModel.getPlayerName(position)
+        val msg = context!!.getString(R.string.save)
+        val posListener = DialogInterface.OnClickListener { _, _ -> viewModel.saveRecord(position) }
+        val negListener = DialogInterface.OnClickListener { _, _ -> adapter.notifyDataSetChanged() }
+        showDialog(title, msg, posListener, negListener)
     }
 
     fun deleteSavedPlayer(position: Int, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
-        val dialogBuilder = AlertDialog.Builder(context)
-        dialogBuilder.setTitle(viewModel.getSavedPlayerName(position))
-        dialogBuilder.setMessage(context!!.getString(R.string.delete))
-        dialogBuilder.setPositiveButton(R.string.confirm) { _, _ ->
-            viewModel.removeSavedPlayer(
-                position
-            )
-        }
-        dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> adapter.notifyDataSetChanged() }
-        dialogBuilder.show()
+
+        val title = viewModel.getSavedPlayerName(position)
+        val msg = context!!.getString(R.string.delete)
+        val posListener = DialogInterface.OnClickListener { _, _ -> viewModel.removeSavedPlayer(position) }
+        val negListener = DialogInterface.OnClickListener { _, _ -> adapter.notifyDataSetChanged() }
+        showDialog(title, msg, posListener, negListener)
     }
 
     fun renameDialog(position: Int) {
@@ -93,6 +102,7 @@ class DialogHelper(private val context: Context?, private val viewModel: MyViewM
         dialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> }
 
         val dialog = dialogBuilder.create()
+        dialog.setCanceledOnTouchOutside(false)
         dialog.setView(view, 40, 100, 40, 0)
         dialog.show()
 
